1ba3a779be2eca2a687f92182b788e2dee8b4fce
marduke182 <jrqb182@gmail.com>
2018-09-11 12:29:12 +0200

    feat: configure react router on root file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '@/shared/containers/App';
+import Categories from '@/categories/containers/Categories';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter>
+    <React.Fragment>
+      <Switch>
+        <Route path="/" component={Categories} exact />
+      </Switch>
+    </React.Fragment>
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
