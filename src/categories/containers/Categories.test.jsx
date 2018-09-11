a98d0e5bfb0d53986db00f79ec69b56e40f60645
marduke182 <jrqb182@gmail.com>
2018-09-11 12:41:15 +0200

    test: add integrity test to category page

---
+++ src/categories/containers/Categories.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Categories from './Categories';
+
+function setup(props = {}) {
+  const wrapper = shallow(<Categories {...props} />);
+  return {
+    wrapper,
+  };
+}
+
+test('should render without throwing or warning', async () => {
+  const consoleSpy = jest.spyOn(console, 'error');
+  expect(setup).not.toThrow();
+
+  expect(consoleSpy).not.toBeCalled();
+});
