9cd1add45ae3354588acbb22fed25eb05fb027ad
marduke182 <jrqb182@gmail.com>
2018-09-12 12:15:42 +0200

    feat: disable category sleepy

--- src/categories/components/Categories.jsx
+++ src/categories/components/Categories.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import CategoryShape from '../libs/CategoryShape';
 import './Categories.scss';
 
+function preventDefault(e) {
+  e.preventDefault();
+}
+
 export default function Categories({ categories }) {
+
   return (
     <div className="Categories Categories--4">
       { categories.map(category => (
-        <Link key={category.id} to={`/category/${category.name}`} className="Category">
+        <Link
+          key={category.id}
+          to={`/category/${category.name}`}
+          className={classnames('Category', {
+            isDisabled: category.sleepy
+          })}
+          onClick={category.sleepy && preventDefault}
+        >
           <img className="Category-image" src={category.openIcon} alt={category.name} />
         </Link>
 
