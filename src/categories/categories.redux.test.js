feb9fda2f4706a695c9a44b48781433cb5d78c9c
marduke182 <jrqb182@gmail.com>
2018-09-12 12:08:17 +0200

    fix: categories tests

--- src/categories/categories.redux.test.js
+++ src/categories/categories.redux.test.js
@@ -1,11 +1,12 @@
 import thunk from 'redux-thunk';
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 
-import glovoClient from '../shared/libs/glovoClient';
+import getCategories from './libs/getCategories';
 import reducer, { actions, selectors, name } from './categories.redux';
 import { actions as storeActions } from '../stores/stores.redux';
 
-jest.mock('../shared/libs/glovoClient');
+jest.mock('./libs/getCategories');
+jest.mock('../stores/libs/getStores');
 jest.mock('../stores/stores.redux');
 
 
@@ -24,10 +25,6 @@ function setup(initialState = {}) {
   };
 }
 
-beforeEach(async () => {
-  jest.spyOn(storeActions, 'getStoresOf').mockImplementation(() => async () => {});
-});
-
 test('should have initial state', async () => {
   const { store } = setup();
   expect(store.getState()).toMatchSnapshot();
@@ -35,11 +32,11 @@ test('should have initial state', async () => {
 
 describe('get categories', () => {
   beforeEach(async () => {
-    glovoClient.get.mockClear();
+    getCategories.mockClear();
   });
 
   test('should show loading', async () => {
-    glovoClient.get.mockReturnValueOnce(new Promise(() => {}));
+    getCategories.mockReturnValueOnce(new Promise(() => {}));
     const { store } = setup();
 
     store.dispatch(actions.getCategoriesAndStores());
@@ -49,43 +46,39 @@ describe('get categories', () => {
 
   describe('success', () => {
 
-    const categoriesResponse = {
-      data: {
-        categories: [
-          {
-            id: 3,
-            label: 'Snacks',
-            name: 'snacks',
-            openIcon: 'http://localhost:3000/img/snacks-open.png',
-            sleepIcon: 'http://localhost:3000/img/snacks-sleep.png'
-          },
-          {
-            id: 2,
-            label: 'Gifts',
-            name: 'gifts',
-            openIcon: 'http://localhost:3000/img/gifts-open.png',
-            sleepIcon: 'http://localhost:3000/img/gifts-sleep.png'
-          },
-          {
-            id: 1,
-            label: 'Restaurants',
-            name: 'restaurants',
-            openIcon: 'http://localhost:3000/img/restaurants-open.png',
-            sleepIcon: 'http://localhost:3000/img/restaurants-sleep.png'
-          },
-          {
-            id: 4,
-            label: 'Wonders',
-            name: 'wonders',
-            openIcon: 'http://localhost:3000/img/wonders-open.png',
-            sleepIcon: 'http://localhost:3000/img/wonders-sleep.png'
-          }
-        ]
+    const categories = [
+      {
+        id: 3,
+        label: 'Snacks',
+        name: 'snacks',
+        openIcon: 'http://localhost:3000/img/snacks-open.png',
+        sleepIcon: 'http://localhost:3000/img/snacks-sleep.png'
+      },
+      {
+        id: 2,
+        label: 'Gifts',
+        name: 'gifts',
+        openIcon: 'http://localhost:3000/img/gifts-open.png',
+        sleepIcon: 'http://localhost:3000/img/gifts-sleep.png'
+      },
+      {
+        id: 1,
+        label: 'Restaurants',
+        name: 'restaurants',
+        openIcon: 'http://localhost:3000/img/restaurants-open.png',
+        sleepIcon: 'http://localhost:3000/img/restaurants-sleep.png'
+      },
+      {
+        id: 4,
+        label: 'Wonders',
+        name: 'wonders',
+        openIcon: 'http://localhost:3000/img/wonders-open.png',
+        sleepIcon: 'http://localhost:3000/img/wonders-sleep.png'
       }
-    };
+    ];
 
     beforeEach(async () => {
-      glovoClient.get.mockImplementation(async () => categoriesResponse);
+      getCategories.mockImplementation(async () => ({categories}));
     });
 
     test('should store categories in store', async () => {
@@ -94,7 +87,7 @@ describe('get categories', () => {
 
       await store.dispatch(actions.getCategoriesAndStores());
 
-      expect(selectors.categories(store.getState())).toEqual(categoriesResponse.data.categories);
+      expect(selectors.categories(store.getState())).toEqual(categories);
     });
   });
 });
